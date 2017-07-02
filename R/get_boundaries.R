c162eccc5cb77e6b70a312848eb08b68a85fdae8
lbusett <lbusett@gmail.com>
2017-07-02 15:49:35 +0200

    add checks on path creation in get_bounadries.R

--- R/get_boundaries.R
+++ R/get_boundaries.R
@@ -2,12 +2,13 @@
 #' @description function to download administrative boundaries data from the gadm archive, starting
 #' from a country name or ISO code, and the level of desired administrtive units
 #' @param iso `character` iso name or 2/3 digits code of the country
-#' @param level `numeric` level of admoinistrative units returned, default: 0 (Country Level)
+#' @param level `numeric` level of administrative units returned, default: 0 (Country Level)
 #' @param path `character`
+#' @param makefold `character`
 #' @return SpatialPolygonsDataFrame containing the desired data
-#' @details the function is a simple wrapper to the raster::getData function, with a-priori checks 
+#' @details the function is a simple wrapper to the raster::getData function, with a-priori checks
 #' on country name or ISO code
-#' @examples 
+#' @examples
 #' \dontrun{
 #' if(interactive()){
 #'  ita_boundaries <- get_boundaries("Italy", 0)
@@ -15,30 +16,31 @@
 #'  plot(ita_region_boundaries)
 #'  }
 #' }
-#' @seealso 
+#' @seealso
 #'  \code{\link[raster]{getData}}
 #' @rdname get_boundaries
-#' @export 
+#' @export
 #' @importFrom raster getData
-get_boundaries <- function(iso, 
-                           level = 1, 
-                           path = tempdir()) {
-  
+get_boundaries <- function(iso,
+                           level    = 1,
+                           path     = NULL,
+                           makefold = TRUE) {
+
   #   ____________________________________________________________________________
   #   check if level is numeric and iso is a recognized country name or ISO code ####
-  
+
   if (!is.numeric(level)) stop("get_boundaries --> level must be numeric. Aborting !")
-  
+
   data(iso3166)
   match_name <- match(iso, iso3166$ISOname)
   if (!is.na(match_name)) {
     iso_code <- maps::iso3166$a3[match_name]
   } else {
-    match_code_i3 <- match(iso, iso3166$a3)  
+    match_code_i3 <- match(iso, iso3166$a3)
     if (!is.na(match_code_i3)) {
-      iso_code <- iso 
+      iso_code <- iso
     } else {
-      match_code_i2 <- match(iso, iso3166$a2)   
+      match_code_i2 <- match(iso, iso3166$a2)
       if (!is.na(match_code_i2)) {
         iso_code <- iso3166$a2[match_code_i2]
       } else {
@@ -46,14 +48,23 @@ get_boundaries <- function(iso,
       }
     }
   }
-  
+
+  if (is.null(path)) {
+    path = tempdir()
+  } else {
+    if (!dir.exists(path) & makefold) {
+      builddir <- try(dir.create(path, recursive = TRUE, showWarnings = FALSE))
+      if (class(builddir == "try-error")) stop("Unable to create the ", path, "folder. Please
+                                               check your inputs and verify file system permissions. Aborting")
+    }
+  }
   #   ____________________________________________________________________________
   #   Download the data                                                       ####
-  
-  adm <- suppressWarnings(try(raster::getData("GADM", 
-                                              country = iso_code, 
-                                              level   = level, 
-                                              path    = path), 
+
+  adm <- suppressWarnings(try(raster::getData("GADM",
+                                              country = iso_code,
+                                              level   = level,
+                                              path    = path),
                               silent = TRUE))
   if (class(adm) == "try-error") {
     stop("get_boundaries --> Download failed. It is possible that the level of data you are trying to download
