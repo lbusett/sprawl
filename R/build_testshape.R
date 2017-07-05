98aabfa58348b3835c35538c7eacddcbdc72b4b1
lbusett <lbusett@gmail.com>
2017-07-05 09:59:48 +0200

    partial improvements in build_testshape
    also removed sprawl_helpers.R

---
+++ R/build_testshape.R
@@ -0,0 +1,81 @@
+#' @title build_testshape
+#' @description FUNCTION_DESCRIPTION
+#' @param maxpolys PARAM_DESCRIPTION
+#' @param allow_overlaps PARAM_DESCRIPTION, Default: FALSE
+#' @param to_file PARAM_DESCRIPTION, Default: FALSE
+#' @return OUTPUT_DESCRIPTION
+#' @details DETAILS
+#' @examples
+#' \dontrun{
+#'  p <- build_testshape(20)
+#   plot(p[1], col = p$id, axes = T)
+#' }
+#' @seealso
+#'  \code{\link[dplyr]{filter}}
+#'  \code{\link[tibble]{as_tibble}}
+#' @rdname build_testshape
+#' @export
+#' @importFrom dplyr filter
+#' @importFrom sf st_as_sf st_buffer st_set_crs st_intersection st_difference st_union
+#' @importFrom tibble as_tibble
+#' @importFrom magrittr "%>%"
+#'
+build_testshape <- function(maxpolys,
+                            rmax           = 10,
+                            allow_overlaps = FALSE,
+                            to_file        = FALSE,
+                            ext            = NULL,
+                            crs            = NULL,
+                            seed           = 100) {
+
+  if (is.null(ext) & is.null(crs)) {
+    x_limits <- c(-180,180)
+    y_limits <- c(-90,90)
+    crs      <- 4326
+  }
+
+  if (!is.null(crs)) {
+    test_crs <- try(sf::st_crs(crs))
+    if (class(test_crs) == "try-error") {
+      stop("build_testshape --> Unrecognized crs. Aborting.")
+    }
+  }
+
+  if (!is.null(extent)) {
+    if (is.numeric(extent) & length(extent) == 4)
+      x_limits <- c(extent[1], extent[3])
+      y_limits <- c(extent[2], extent[4])
+  }
+
+  xy <- data.frame(
+    id = paste0("id_", 1:maxpolys),
+    x = stats::runif(maxpolys, min(x_limits), max(x_limits)),
+    y = stats::runif(maxpolys, min(y_limits), max(y_limits))) %>%
+    tibble::as_tibble()
+  polys <- sf::st_as_sf(xy, coords = c(2,3)) %>%
+    sf::st_buffer(stats::runif(maxpolys, min = 2, max = rmax)) %>%
+    sf::st_set_crs(4326)
+  if (!allow_overlaps) {
+    for (i in 1:dim(polys)[1]) {
+      cur_pol <- polys[i,]
+      intersections <- sf::st_intersection(cur_pol, sf::st_union(polys[-i,]))
+      if (dim(intersections)[1] != 0) {
+        polys[i,] <- sf::st_difference(cur_pol, st_union(intersections))[1,1]
+      }
+      # res_poly <- sf::st_difference(res_poly, polys[i,])
+    }
+  }
+  #   int <- sf::st_intersection(polys, polys) %>%
+  #     dplyr::filter(id != id.1) %>%
+  #     sf::st_as_sf()
+  #   if (length(int$id) > 0) polys <- sf::st_difference(x, sf::st_union(st_combine(y)))
+  # }
+
+  if (to_file) {
+    tempfilename <- tempfile(fileext = ".shp")
+    tempfile <- writeshape(polys, out_file = tempfilename)
+    return(tempfilename)
+  } else {
+    return(polys)
+  }
+}
