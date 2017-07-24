f83a8be8250911bb7ba7ffb8bd84bb882625eadf
lbusett <lbusett@gmail.com>
2017-07-24 19:13:47 +0200

    Modified check_spatype as s3 method

--- R/check_spatype.R
+++ R/check_spatype.R
@@ -4,7 +4,7 @@
 #' or a file corresponding to a raster
 #' @param object either a `R` object or a `character` string pointing to a vector or raster layer
 #' @return character vector equal to *spobject*, *sfobject*, *rastobject*, *vectfile* or *rastfile*
-#'
+#' @rdname check_spatype
 #' @export
 #' @importFrom gdalUtils ogrinfo gdalinfo
 #' @examples \dontrun{
@@ -13,56 +13,87 @@
 #' check_spatype(system.file("extdata","testrast.tif", package = "sprawl.data"))
 #'
 #' # input is a shapefile
-#' shp_file <- system.file("extdata","lc_polys.shp", package = "sprawl.data")
+#' shp_file             <- system.file("extdata","lc_polys.shp", package = "sprawl.data")
 #' check_spatype(shp_file)
 #'
 #' # input is a `sp` object
-#' obj <- read_shape(shp_file, as_sp = TRUE)
+#' obj                  <- read_shape(shp_file, as_sp = TRUE)
 #' check_spatype(obj)
 #'
 #' # input is a `sf` object
-#' obj <- read_shape(shp_file)
+#' obj                  <- read_shape(shp_file)
 #' check_spatype(obj)
 #' }
 #'
-check_spatype <- function(object) {
+# check_spatype_met     <- function(object) {
+#   UseMethod("check_spatype", object)
+# }
+#
+# check_spatype.default <- function(object) {
+#   stop("none")
+# }
+#
+#
 
-  sp_type <- "none"
-
-  if (inherits(object,"Spatial")) {sp_type <- "spobject"}
-
-  if (inherits(object,"sf") | inherits(object,"sfc"))      {sp_type <- "sfobject"}
-
-  if (inherits(object,"Raster"))  {sp_type <- "rastobject"}
-
-  if (sp_type == "none") {
-
-    if (class(object) == "character") {
-
-      if (file.exists(object)){
-
-        vecttry <- suppressWarnings(try(gdalUtils::ogrinfo(object, al = TRUE, so = TRUE), silent = TRUE))
-
-        if (is.null(attr(vecttry, "status"))) {
-          sp_type <- "vectfile"
-
-        } else {
-
-          rastry <- suppressWarnings(try(gdalUtils::gdalinfo(object), silent = TRUE))
-
-          if (is.null(attr(rastry, "status"))) {
-
-            sp_type <- "rastfile"
-
-          } else {
-            sp_type <- "none"
-          }
-        }
-      }
-
-    } else {
-      sp_type <- "none"
-    }
-  }
-  return(sp_type)
+check_spatype  <- function(object) {
+  UseMethod("check_spatype")
 }
+
+#   ____________________________________________________________________________
+#   Fallback method                                                         ####
+
+check_spatype.default   <- function(object) {
+  "none"
+}
+
+#   ____________________________________________________________________________
+#   Method for "character" - find if file exists and is "spatial"           ####
+
+check_spatype.character <- function(object) {
+  if (file.exists(object)) {
+    vecttry             <- suppressWarnings(try(gdalUtils::ogrinfo(object, al = TRUE, so = TRUE), silent = TRUE))
+    if (is.null(attr(vecttry, "status"))) {
+      "vectfile"
+    } else {
+      rastry  <- suppressWarnings(try(gdalUtils::gdalinfo(object), silent = TRUE))
+      if (is.null(attr(rastry, "status"))) {
+        "rastfile"
+      } else {
+        "none"
+      }
+    }
+  } else {
+    "none"
+  }
+}
+
+
+#   ____________________________________________________________________________
+#   Method for "Raster"                                                     ####
+
+check_spatype.Raster    <- function(object) {
+  "rastobject"
+}
+
+#   ____________________________________________________________________________
+#   Method for "sf"                                                         ####
+
+check_spatype.sf        <- function(object) {
+  "sfobject"
+}
+
+#   ____________________________________________________________________________
+#   Method for "sfc"                                                         ####
+
+check_spatype.sfc        <- function(object) {
+  "sfobject"
+}
+
+
+#   ____________________________________________________________________________
+#   Method for "Spatial"                                                    ####
+
+check_spatype.Spatial   <- function(object) {
+  "spobject"
+}
+
