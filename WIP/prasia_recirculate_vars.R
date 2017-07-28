c56a99e6bd7a14da88fd50372cb167840dea6040
lbusett <lbusett@gmail.com>
2017-07-28 17:19:59 +0200

    various

--- WIP/prasia_recirculate_vars.R
+++ WIP/prasia_recirculate_vars.R
@@ -1,34 +1,35 @@
 library(sprawl)
 library(sf)
+library(dplyr)
+library(tidyr)
 
-in_folder <- "/home/lb/my_data/prasia/mosaics/"
+mosaics_folder <- "/home/lb/my_data/prasia/mosaics/by_year"
+
+in_sos_files <- list.files(mosaics_folder, pattern = "sos*", full.names = T)
+
+names <- tibble::tibble(fullname = in_sos_files,
+                        shortname = basename(tools::file_path_sans_ext(in_sos_files)))
+split_names <- stringr::str_split_fixed(names$shortname, "_", 3)
+
+names <- cbind(names, split_names, or_order = seq(1:length(names$fullname))) %>%
+  `colnames<-`(c("fullname", "basename", "var", "season", "year", "order")) %>%
+ dplyr::arrange(year, season)
+
+ordered_files <- names$fullname
+
+temp_vrt <- tempfile(fileext = ".vrt")
+pp = gdalUtils::gdalbuildvrt(ordered_files, temp_vrt, separate = T)
 
 
-
-#   ____________________________________________________________________________
-#   create full raster stacks and cahnge band names                         ####
-
-in_rast_nseas  <- raster::stack(list.files(in_folder, pattern = glob2rx("nseas*"), full.names = T))
-in_rast_sos    <- raster::stack(list.files(in_folder, pattern = glob2rx("sos*"), full.names = T))
-in_rast_eos    <- raster::stack(list.files(in_folder, pattern = glob2rx("eos*"), full.names = T))
-in_rast_pos    <- raster::stack(list.files(in_folder, pattern = glob2rx("pos*"), full.names = T))
-in_rast_veglgt <- raster::stack(list.files(in_folder, pattern = glob2rx("veg*"), full.names = T))
-in_rast_totlgt <- raster::stack(list.files(in_folder, pattern = glob2rx("tot*"), full.names = T))
-in_rast_cumevi <- raster::stack(list.files(in_folder, pattern = glob2rx("cum*"), full.names = T))
+ gdal_translate -ot Int16 -of Gtiff -co "COMPRESS=DEFLATE" /tmp/RtmpyrONRv/file7360614d92a6.vrt /tmp/RtmpyrONRv/prova.tif
 
 
+test_out <- file.path("/home/lb/my_data/prasia/mosaics/tests/test1.tif")
+dir.create(dirname(test_out))
+test <- raster::writeRaster(in_rast_nseas[[1:5]], test_out, options = c("COMPRESS=DEFLATE"))
 
-years = seq(2003,2016)
-for (y_ind in seq_along(years)) {
-  yy = years[y_ind]
-  names(in_rast_sos)    <- gsub(paste0("_all.",y_ind, "$"), paste0("_", yy), names(in_rast_sos))
-  names(in_rast_eos)    <- gsub(paste0("_all.",y_ind, "$"), paste0("_", yy), names(in_rast_eos))
-  names(in_rast_pos)    <- gsub(paste0("_all.",y_ind, "$"), paste0("_", yy), names(in_rast_pos))
-  names(in_rast_veglgt) <- gsub(paste0("_all.",y_ind, "$"), paste0("_", yy), names(in_rast_veglgt))
-  names(in_rast_totlgt) <- gsub(paste0("_all.",y_ind, "$"), paste0("_", yy), names(in_rast_totlgt))
-  names(in_rast_cumevi) <- gsub(paste0("_all.",y_ind, "$"), paste0("_", yy), names(in_rast_cumevi))
-  names(in_rast_nseas)  <- gsub(paste0("_all.",y_ind, "$"), paste0("_", yy), names(in_rast_nseas ))
-}
+test_vrt <- tempfile(fileext = ".vrt")
+gdalUtils::gdalbuildvrt(in_rast_nseas[[1:5]], test_vrt)
 
 
 #   ____________________________________________________________________________
