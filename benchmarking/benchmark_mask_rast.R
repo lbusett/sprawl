529a8b0bfaf10ea0914d8a79f8c83c39b3684a2c
lbusett <lbusett@gmail.com>
2017-08-04 18:43:32 +0200

    benchmarks on mask_rast

--- benchmarking/benchmark_mask_rast.R
+++ benchmarking/benchmark_mask_rast.R
@@ -17,3 +17,41 @@ bench <- microbenchmark::microbenchmark(
   "sprawl::mask_rast" = {out_mask_raster  <- mask_rast(in_rast, in_vect, crop = FALSE)},
   times = 5)
 boxplot(bench)
+
+rast_dims <- c(500,1000,2000)
+n_polys   <- c(1, 10, 100)
+n_bands   <- c(1, 5, 10)
+benches <- list()
+index <- 0
+
+for (dim in rast_dims) {
+
+  for (polys in n_polys) {
+
+    for (bands in n_bands) {
+
+    message(dim, " ", polys, " ", bands )
+    index <- index + 1
+    tmprast <- tempfile(fileext = ".tif")
+    in_rast <- build_testraster(dim, dim , bands) %>%
+      raster::writeRaster(filename = tmprast, overwrite = T)
+
+    in_vect <- create_fishnet(in_rast, pix_for_cell = dim/10) %>%
+      dplyr::sample_n(polys)
+    in_vect <- sf::st_set_crs(in_vect, get_projstring(in_rast))
+    sp_polys <- as(in_vect, "Spatial")
+
+    bench <- microbenchmark::microbenchmark(
+      "raster::mask" = {out_mask_raster  <- raster::mask(in_rast, sp_polys)},
+      "sprawl::mask_rast" = {out_mask_raster  <- mask_rast(in_rast, in_vect, crop = FALSE)},
+      times = 1)
+    bench <- summary(bench) %>%
+      mutate(n_bands = bands, n_polys = polys, rast_dim = dim )
+    benches[[index]] <- bench
+
+    }
+
+  }
+}
+
+
