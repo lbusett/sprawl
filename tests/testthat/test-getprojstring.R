45deb1dcea733c60b2296484e738be5c7e301206
ggranga <ggranga@gmail.com>
2017-08-21 13:51:49 +0200

    Fix projstring -> prooj4string

--- tests/testthat/test-getprojstring.R
+++ tests/testthat/test-getprojstring.R
@@ -4,41 +4,41 @@ testthat::test_that("Test projection string",{
   skip_on_travis()
   # no proj set
   library(sprawl.data)
-  testthat::expect_error(get_projstring(in_pts))
+  testthat::expect_error(get_proj4string(in_pts))
 
-  # extraction of projstring from raster ----
+  # extraction of proj4string from raster ----
   in_rast <- system.file("extdata", "sprawl_EVItest.tif", package = "sprawl.data")
-  expect_equal(get_projstring(in_rast),
+  expect_equal(get_proj4string(in_rast),
                "+proj=sinu +lon_0=0 +x_0=0 +y_0=0 +a=6371007.181 +b=6371007.181 +units=m +no_defs")
 
-  expect_equal(get_projstring(raster::raster(in_rast)),
+  expect_equal(get_proj4string(raster::raster(in_rast)),
                "+proj=sinu +lon_0=0 +x_0=0 +y_0=0 +a=6371007.181 +b=6371007.181 +units=m +no_defs")
 
-  # extraction of projstring from vector ----
+  # extraction of proj4string from vector ----
   in_vect <- system.file("extdata","lc_polys.shp", package = "sprawl.data")
 
-  expect_equal(get_projstring(in_vect),
+  expect_equal(get_proj4string(in_vect),
                "+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0")
 
-  expect_equal(get_projstring(read_vect(in_vect)),
+  expect_equal(get_proj4string(read_vect(in_vect)),
                "+proj=longlat +datum=WGS84 +no_defs")
 
-  expect_equal(get_projstring(read_vect(in_vect, as_sp = TRUE)),
+  expect_equal(get_proj4string(read_vect(in_vect, as_sp = TRUE)),
                "+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0")
 
 
-  # warning/abort on wrong projstring ----
+  # warning/abort on wrong proj4string ----
   rastobj <- raster::raster(in_rast)
   expect_warning(raster::crs(rastobj) <- "+proj=lcc +lat_1=48 +lat_2=33 +lon_0=-100 +ellps=WGSìèàrwa4")
 
-  expect_warning(out <- get_projstring(rastobj))
+  expect_warning(out <- get_proj4string(rastobj))
   expect_equal(out, "invalid")
-  expect_error(get_projstring(rastobj, abort = TRUE))
+  expect_error(get_proj4string(rastobj, abort = TRUE))
 
   # warning/abort on invalid filename or object ----
-  expect_warning(out <- get_projstring("pippo.shp"))
+  expect_warning(out <- get_proj4string("pippo.shp"))
   expect_equal(out, "none")
-  expect_error(get_projstring(123, abort = TRUE))
+  expect_error(get_proj4string(123, abort = TRUE))
 
 }
 )
