f1f7927d8f6b38c0e34f854bbf2bb4416761a064
lbusett <lbusett@gmail.com>
2017-08-23 11:58:49 +0200

    Fixed missing imports by using ::

--- tests/testthat/test_reproj_extent.R
+++ tests/testthat/test_reproj_extent.R
@@ -21,27 +21,28 @@ testthat::test_that("Test reproj_extent",{
   testthat::expect_equal(ext_reproj_2, ext_reproj_1)
 
   # All ok - reproj from extent and in_proj
-  ext_reproj_3 <- reproj_extent(extent(in_rast), "+init=epsg:32651", in_proj=in_rast@crs)
+  ext_reproj_3 <- reproj_extent(raster::extent(in_rast),
+                                "+init=epsg:32651", in_proj=in_rast@crs)
   testthat::expect_is(ext_reproj_3, "sprawlext")
   testthat::expect_equal(ext_reproj_3, ext_reproj_1)
 
   # All ok - reproj without enlarging
-  ext_reproj_4 <- reproj_extent(in_sprawlext_1, "+init=epsg:32651", enlarge = FALSE)
-  expect_equal(st_covers(as(ext_reproj_1,"sfc_POLYGON"), as(ext_reproj_4,"sfc_POLYGON"), sparse=FALSE),
+  ext_reproj_4 <- reproj_extent(in_sprawlext_1, "+init=epsg:32651",
+                                enlarge = FALSE)
+  expect_equal(sf::st_covers(as(ext_reproj_1,"sfc_POLYGON"),
+                             as(ext_reproj_4,"sfc_POLYGON"),
+                             sparse = FALSE),
                matrix(TRUE))
 
   # Error - no proj correctly set
   testthat::expect_error(
     reproj_extent(in_sprawlext_1, "+init=epsg:432ds6"))
   testthat::expect_error(
-    reproj_extent(extent(in_rast), "+init=epsg:432ds6",
-                  in_proj = "+init=epsg:3sss035"))  #nolint
+    reproj_extent(extent(in_rast), "+init=epsg:432ds6", in_proj = "+init=epsg:3sss035"))  #nolint
 
   # Error - given bounding box but missing in_proj
   testthat::expect_error(
     reproj_extent(bbox(in_rast), "+init=epsg:32651"))
-
-
 
   # these was skipped since a common reproj function will be done (for now, output is always a sprawlext)
 
