1f9cad40a3a2ca3d5c91af70f084194d7576d7df
lbusett <lbusett@gmail.com>
2017-09-04 12:33:49 +0200

    add sprawl.data in description, skip plot_rast_gg test

--- tests/testthat/test_plot_rast_gg.R
+++ tests/testthat/test_plot_rast_gg.R
@@ -3,9 +3,9 @@ context("plot_rast_gg")
 
 test_that("plot_rast_gg works as expected", {
 
-  # skip_on_travis()
-  in_rast <- read_rast(system.file("extdata/OLI_test", "oli_multi_1000.tif",
-                                       package = "sprawl.data"))[[1]]
+  skip_on_travis()
+  in_rast <- read_rast(system.file("extdata/OLI_test", "oli_multi_1000_b1.tif",
+                                       package = "sprawl.data"))
   p <- plot_rast_gg(in_rast, scalebar_dist = 20)
   expect_is(p, "gg")
 
