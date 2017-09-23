a0a12b772d45deacacefa1e2621440c2c1e43e33
lbusett <lbusett@gmail.com>
2017-09-23 08:50:58 +0200

    more tests on travis

--- tests/testthat/test_cast_vect.R
+++ tests/testthat/test_cast_vect.R
@@ -3,7 +3,7 @@ context("Cast vector to other type")
 test_that("cast_vect works as expected", {
   library(testthat)
   skip_on_travis()
-  # skip_on_cran()
+  skip_on_cran()
   library(sprawl.data)
 
   # input is a shapefile
