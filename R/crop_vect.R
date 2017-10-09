0188b1f2a5718b12f7082e6239643bcbe48caafe
lbusett <lbusett@gmail.com>
2017-10-09 12:19:06 +0200

    Added out_file save on crop_vect

--- R/crop_vect.R
+++ R/crop_vect.R
@@ -1,10 +1,13 @@
 #' @title crop a vector on the extent of a different spatial file
 #' @description Function allowing to crop an input vector object or shapefile on the extent of a
-#' different spatial object. in_vect must be an *sp, *sf object or vector file, while in_obj should
+#' different spatial object. in_vect must be an *sp, *sf object or vector file, while ext_obj should
 #' be a valid spatial object (sp, sf, vector file, raster or raster file name)
-#' @param in_vect PARAM_DESCRIPTION
-#' @param in_obj PARAM_DESCRIPTION
-#' @param out_file PARAM_DESCRIPTION, Default: FALSE
+#' @param in_vect Object of class `sfc_POLYGON` or `sf_POLYGON`, or coercible to
+#'   it using (sprawl::cast_vect)
+#' @param ext_obj Object of class `sprawl_ext`, or from which an extent can be
+#'   retrieved using `spralw::get_extent` (i.e., any `R` spatial object, or
+#'   valid filenames of raster or vector files)
+#' @param out_file PARAM_DESCRIPTION, Default: NULL
 #' @param verbose PARAM_DESCRIPTION, Default: FALSE
 #' @return OUTPUT_DESCRIPTION
 #' @details DETAILS
@@ -36,7 +39,7 @@
 #' @importFrom sp proj4string
 #'
 crop_vect <- function(in_vect,
-                      in_obj,
+                      ext_obj,
                       out_file = NULL,
                       verbose  = TRUE) {
 
@@ -48,7 +51,7 @@ crop_vect <- function(in_vect,
   #   __________________________________________________________________________
   #   Check arguments                                                       ####
   vectype <- get_vectype(in_vect)
-  objtype <- get_spatype(in_obj)
+  objtype <- get_spatype(ext_obj)
 
   if (vectype == "sprawlext") {
     in_vect <- as(in_vect, "sfc_POLYGON")
@@ -56,9 +59,9 @@ crop_vect <- function(in_vect,
     in_vect <- cast_vect(in_vect, "sfobject")
   }
 
-  obj_bbox    <- get_extent(in_obj)
+  obj_bbox    <- get_extent(ext_obj)
   invect_proj <- get_proj4string(in_vect)
-  inobj_proj  <- get_proj4string(in_obj)
+  inobj_proj  <- get_proj4string(ext_obj)
   obj_boundaries <- as(obj_bbox, "sfc_POLYGON")
   if (invect_proj != inobj_proj) {
     obj_boundaries <- sf::st_transform(obj_boundaries, invect_proj)
@@ -68,5 +71,9 @@ crop_vect <- function(in_vect,
     sf::st_agr(in_vect) = "constant"
   }
   cropped_vect <- sf::st_intersection(in_vect, obj_boundaries)
+
+  if (!is.null(out_file)) {
+    write_shape(cropped_vect, out_file)
+  }
   return(cropped_vect)
 }
